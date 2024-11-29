3c8e3790ef0d8cd43596d52c3489a9174bc316db
NandhanN <nandhan.p@kriyadocs.com>
2024-11-29 16:28:30 +0530

    login.py file

---
+++ login.py
@@ -0,0 +1,14 @@
+class Animal:
+    def dog(self):
+        print("The cutest tale for the ")
+    def Cat(self):
+        print("The food was made for")
+
+class Human(Animal):
+    def Ram(self):
+        print("He was very goood in Maths")
+        
+V1= Human()
+V1.dog()
+V1.Cat()
+V1.Ram()
